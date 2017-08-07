feb2036c5e1cf9cf4d882328dae536267ff4f7e1
Lupco Kotev <lupco.kotev@keitaro.info>
2017-08-07 21:41:59 +0200

    Add query entity

---
+++ src/main/java/mk/ukim/finki/wbs/csparqlweb/model/Query.java
@@ -0,0 +1,58 @@
+package mk.ukim.finki.wbs.csparqlweb.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class Query {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String definition;
+
+    @JsonIgnore
+    @ManyToOne
+    private User user;
+
+    protected Query() {
+    }
+
+    public Query(String definition) {
+        this.definition = definition;
+    }
+
+    @Override
+    public String toString() {
+        return "Query{" +
+                "id=" + id +
+                ", definition='" + definition + '\'' +
+                '}';
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
