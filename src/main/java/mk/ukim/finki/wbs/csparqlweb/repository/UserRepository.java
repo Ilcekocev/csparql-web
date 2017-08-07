89af63a6153860472fbb48953890b3ff7211daf1
Lupco Kotev <lupco.kotev@keitaro.info>
2017-08-07 21:42:35 +0200

    Add user repository

---
+++ src/main/java/mk/ukim/finki/wbs/csparqlweb/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wbs.csparqlweb.repository;
+
+import mk.ukim.finki.wbs.csparqlweb.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
